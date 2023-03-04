745352e4e5d0af218b034d2e767e01a733abab04
K0N0HA <107436295+K0N0HATV@users.noreply.github.com>
2023-03-04 06:40:31 +0200

    add product

--- shopify/assets/swiper.js
+++ shopify/assets/swiper.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper1", {
     cssMode: true,
     breakpoints: {
         600: {
@@ -7,15 +7,59 @@ var swiper = new Swiper(".mySwiper", {
     },
     spaceBetween: 10,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination1",
     },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
+    mousewheel: true,
+    keyboard: true,
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+var swiper = new Swiper(".mySwiper3", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next3",
+        prevEl: ".swiper-button-prev3",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
+
+var swiper = new Swiper(".mySwiper4", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next4",
+        prevEl: ".swiper-button-prev4",
     },
     mousewheel: true,
     keyboard: true,
